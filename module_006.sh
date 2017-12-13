1a349e17d3b8cb41ad950bf607f1b29977da6cea
Md. Farhabi Helal <faheyon@yahoo.com>
2017-12-13 10:54:55 +0600

    Updated module_006.sh

--- module_006.sh
+++ module_006.sh
@@ -1,14 +1,27 @@
 #! /bin/bash
 
-# Exit status
+# Special parameters
 # http://www.freeos.com/guides/lsst/ch02sec09.html
+# http://www.gnu.org/software/bash/manual/html_node/Special-Parameters.html#Special-Parameters
+
+
+
+# $*    = Expands to the positional parameters, starting from one.
+# $@    = Expands to the positional parameters, starting from one.
+# $#    = Expands to the number of positional parameters in decimal.
+# $?    = Expands to the exit status of the most recently executed foreground pipeline. 
+# $0    = Expands to the name of the shell or shell script.
+
+
+
+# Getting the name of the script
+echo -e "\nMy name is $0.\n"
 
 
 
 # Successful command returns 0
 ls -l --color=auto
 echo -e "\nExit status : $?\n\n"
-
 
 # Unsuccessful command returns non 0
 ls dasdasdaasdasdasda
